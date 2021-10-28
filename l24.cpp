2de4db3f36dcecb1aebb5e093b957bc1d8482db2
easy <60936497+ezberlin@users.noreply.github.com>
2021-10-28 22:08:17 +0200

    extending exercise

--- l24.cpp
+++ l24.cpp
@@ -6,6 +6,8 @@ int  DemoConsoleOutput()
 	cout << "This is a simple string literal" << endl;
 	cout << "Writing number eleven: " << 11 << endl;
 	cout << "Performing division 110 / 11 = " << 110 / 11 << endl;
+	cout << "22 - 7 = " << 22 - 7 << endl;
+	cout << "22 * 7 = " << 22 * 7 << endl;
 	cout << "Pi when approximated is 22 / 7 = " << 22 / 7 << endl;
 	cout << "Pi actually is  22 / 7 = " << 22.0 / 7 << endl;
 
