21aeceb31a951f4b06956f95a33dc80bb625e6d6
easy <60936497+ezberlin@users.noreply.github.com>
2021-10-30 23:35:42 +0200

    new program about void and functios

---
+++ l32.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std; 
+
+void MultiplyNumbers()
+{
+	cout << "This program will help you multiply two numbers" << endl;
+
+	cout << "Enter the first number: ";
+	int firstNumber = 0, secondNumber = 0, multiplicationResult = 0;
+	cin >> firstNumber;
+
+	cout << "Enter the second number: ";
+	cin >> secondNumber;
+
+	//here we store the result
+	multiplicationResult = firstNumber * secondNumber;
+
+	cout << firstNumber << " x " << secondNumber;
+	cout << " = " << multiplicationResult << endl;
+
+}
+int main ()
+{
+	cout << "This program will help you multiply two numbers" << endl;
+	MultiplyNumbers();
+	return 0;
+}
