ef9a389b17fbe21cbafcb9d7299455fdcf34c595
easy <60936497+ezberlin@users.noreply.github.com>
2021-10-31 11:59:39 +0100

    global variables

---
+++ l33.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std; 
+
+int firstNumber = 0;
+int secondNumber = 0;
+int multiplicationResult = 0;
+
+void MultiplyNumbers()
+{
+
+	cout << "Enter the first number: ";
+	cin >> firstNumber;
+
+	cout << "Enter the second number: ";
+	cin >> secondNumber;
+
+	//here we store the result
+	multiplicationResult = firstNumber * secondNumber;
+
+	cout << firstNumber << " x " << secondNumber << " = " << multiplicationResult << endl;
+
+}
+int main ()
+{
+	cout << "This program will help you multiply two numbers" << endl;
+	MultiplyNumbers();
+	cout << "Displaying from main() : ";
+	cout << firstNumber << " x " << secondNumber;
+	cout << " = " << multiplicationResult << endl;
+	return 0;
+}
