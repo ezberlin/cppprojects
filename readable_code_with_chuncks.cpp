5d0e87fd1b0c91c7892a2b2d066d016e42335cf2
easy <60936497+ezberlin@users.noreply.github.com>
2021-11-13 14:44:29 +0100

    using chuncks for readable code

---
+++ readable_code_with_chuncks.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int moneyInBank = 300'000;
+    float pi = 3.141'592'65;
+    cout << "The number pi is ca. " << pi << " and the money in bank equals " << moneyInBank << "." <<endl;
+
+    return 0;
+}
