664341bb741f56b465a572bdffd4aaa496d88444
easy <60936497+ezberlin@users.noreply.github.com>
2021-11-13 15:24:11 +0100

    auto variable types

---
+++ auto.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdint>
+using namespace std;
+
+int main()
+{
+    auto coinFlippedHeads {true};
+    auto largeNumber {250000000000000};
+
+    cout << "coinFlippedHeads = " << coinFlippedHeads;
+
+    cout << " , sizeof(coinFlippedHeads) = " << sizeof(coinFlippedHeads) << endl;
+
+    cout << "largeNumber = " << largeNumber;
+
+    cout << " , sizeof(largeNumber) = " << sizeof(largeNumber) << endl;
+    
+    return 0;
+}
