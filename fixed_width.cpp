d7a2784428f2ccaaedd39f25e61d8d621e6ad22d
easy <60936497+ezberlin@users.noreply.github.com>
2021-11-13 15:11:48 +0100

    list initialization

--- fixed_width.cpp
+++ fixed_width.cpp
@@ -4,9 +4,16 @@ using namespace std;
 
 int main()
 {
-    uint64_t moneyInBank = 300'000;
-    float pi = 3.141'592'65;
-    cout << "The number pi is ca. " << pi << " and the money in bank equals " << moneyInBank << "." <<endl;
+    uint64_t moneyInBank {9'000'000} ;
+    uint16_t smallMoney {moneyInBank}; //warning
+    double pi = 3.141'592'65000000000000000000000000000000000000000000000000000000000000000000000000000000001;
+    float pi2 {pi};
+
+    cout <<  "The money in bank equals " << moneyInBank << "." <<endl;
+    cout <<  "The small money is " << smallMoney << "." <<endl;
+    cout <<  "double Pi is " << pi << "." <<endl;
+    cout <<  "float Pi is " << pi2 << "." <<endl;
+
 
     return 0;
 }
