118958a29baedbe7431bae656e17c9a15075fe04
easy <60936497+ezberlin@users.noreply.github.com>
2021-11-13 14:55:04 +0100

    using chuncks for readable code with fixed width

---
+++ fixed_width.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdint>
+using namespace std;
+
+int main()
+{
+    uint64_t moneyInBank = 300'000;
+    float pi = 3.141'592'65;
+    cout << "The number pi is ca. " << pi << " and the money in bank equals " << moneyInBank << "." <<endl;
+
+    return 0;
+}
