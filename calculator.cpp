b3e23e421691806dda1b6a99821e187f6b47bac1
easy <60936497+ezberlin@users.noreply.github.com>
2022-03-16 18:40:45 +0100

    calculator with optional repetition

--- calculator.cpp
+++ calculator.cpp
@@ -1,40 +1,44 @@
 #include <iostream>
 using namespace std;
-int result = 0;
+int again = 1;
 int main()
 {
-	int operation = 0;
-	int firstNumber = 0;
-	int secondNumber = 0;
+	while (again == 1)
+	{
+		int operation = 0;
+		int firstNumber = 0;
+		int secondNumber = 0;
 
-	cout << "Enter first number: " << endl;
-	cin >> firstNumber;
-	cout << "Enter operation: (addition=1, subtraction=2, multiplication=3, division=4)" << endl;
-	cin >> operation;
-	cout << "Enter second number: " << endl;
-	cin >> secondNumber;
+		cout << "Enter first number: ";
+		cin >> firstNumber;
+		cout << "Enter operation: (addition=1, subtraction=2, multiplication=3, division=4)" << endl;
+		cin >> operation;
+		cout << "Enter second number: " << endl;
+		cin >> secondNumber;
 
-	if (operation == 1)
-	{
-		int result = firstNumber + secondNumber;
-		cout << "The result is " << result << endl;
+		if (operation == 1) //addition
+		{
+			int result = firstNumber + secondNumber;
+			cout << "The result is " << result << endl;
+		}
+		else if(operation == 2) //subtraction
+		{
+			int result = firstNumber - secondNumber;
+			cout << "The result is " << result << endl;
+		}
+		else if(operation == 3) //multiplication
+		{
+			int result = firstNumber * secondNumber;
+			cout << "The result is " << result << endl;
+		}
+		else if(operation == 4 && firstNumber != 0 && secondNumber != 0)
+		{
+			float result = firstNumber / secondNumber;
+			cout << "The result is " << result << endl;
+		}
+		cout << "Do you want to return? (0/1)" << endl;
+		cin >> again;
 	}
-	else if(operation == 2)
-	{
-		int result = firstNumber - secondNumber;
-		cout << "The result is " << result << endl;
-	}
-	else if(operation == 3)
-	{
-		int result = firstNumber * secondNumber;
-		cout << "The result is " << result << endl;
-	}
-	else if(operation == 4 && firstNumber != 0 && secondNumber != 0)
-	{
-		float result = firstNumber / secondNumber;
-		cout << "The result is " << result << endl;
-	}
-
 	
 	return 0;
 }
