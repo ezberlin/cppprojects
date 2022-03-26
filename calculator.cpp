d1da781ab9f9284ac0ec8e7798b25bd3d9d7ac3f
easy <60936497+ezberlin@users.noreply.github.com>
2022-03-26 21:32:31 +0100

    work not complete yet

--- calculator.cpp
+++ calculator.cpp
@@ -6,15 +6,19 @@ int main()
 	while (again == 1)
 	{
 		int operation = 0;
-		int firstNumber = 0;
-		int secondNumber = 0;
+		long long firstNumber = 0;
+		long secondNumber = 0;
 
 		cout << "Enter first number: ";
 		cin >> firstNumber;
-		cout << "Enter operation: (addition = 1, subtraction = 2, multiplication = 3, division = 4, test of relation = 5)" << endl;
+		cout << "Enter operation: (addition = 1, subtraction = 2, multiplication = 3, division = 4, test of relation = 5, lenght convertor = 6)" << endl;
 		cin >> operation;
-		cout << "Enter second number: " << endl;
-		cin >> secondNumber;
+		if (operation != 6)
+		{
+			cout << "Enter second number: " << endl;
+			cin >> secondNumber;
+		}
+		
 
 		if (operation == 1) //addition
 		{
@@ -49,6 +53,134 @@ int main()
 			if (firstNumber > secondNumber)
 			{
 				cout << "The first number is greater than the second" << endl;
+			}
+		}
+		else if(operation == 6)
+		{
+			int startUnit = 0;
+			int resultUnit = 0;
+
+			cout << "What's the start unit? (lightyear = 1, AE = 2, mile = 3, kilometres = 4, meters = 5, foot = 6, inch = 7, centimeters = 8, millimetres = 9)" << endl;
+			cin >> startUnit;
+			cout << "What's the result unit? (lightyear = 1, AE = 2, mile = 3, kilometres = 4, meters = 5, foot = 6, inch = 7, centimeters = 8, millimetres = 9)" << endl;
+			cin >> resultUnit;
+
+			if (startUnit == 1)
+			{
+				if resultUnit == 1
+				{
+					cout << "That are " << firstNumber << " lightyears" << endl;
+				}
+				if resultUnit == 2
+				{
+					cout << "That are " << 63241 * firstNumber << " astronomical units" << endl;
+				}
+				if resultUnit == 3
+				{
+					cout << "Too big result" << endl;
+				}
+				if resultUnit == 4
+				{
+					cout << "Too big result" << endl;
+				}
+				if resultUnit == 5
+				{
+					cout << "Too big result" << endl;
+				}
+				if resultUnit == 6
+				{
+					cout << "Too big result" << endl;
+				}
+				if resultUnit == 7
+				{
+					cout << "Too big result" << endl;
+				}
+				if resultUnit == 8
+				{
+					cout << "Too big result" << endl;
+				}
+				if resultUnit == 9
+				{
+					cout << "Too big result" << endl;
+				}
+			}
+			if (startUnit == 2)
+			{
+				if resultUnit == 1
+				{
+					cout << "That are " << firstNumber / 63241 << " lightyears" << endl;
+				}
+				if resultUnit == 2
+				{
+					cout << "That are " << firstNumber << " astronomical units" << endl;
+				}
+				if resultUnit == 3
+				{
+					cout << "Too big result" << endl;
+				}
+				if resultUnit == 4
+				{
+					cout << "Too big result" << endl;
+				}
+				if resultUnit == 5
+				{
+					cout << "Too big result" << endl;
+				}
+				if resultUnit == 6
+				{
+					cout << "Too big result" << endl;
+				}
+				if resultUnit == 7
+				{
+					cout << "Too big result" << endl;
+				}
+				if resultUnit == 8
+				{
+					cout << "Too big result" << endl;
+				}
+				if resultUnit == 9
+				{
+					cout << "Too big result" << endl;
+				}
+			}
+			if (startUnit == 3)
+			{
+				if resultUnit == 1
+				{
+					cout << "To low result" << endl;
+				}
+				if resultUnit == 2
+				{
+					cout << "To low result" << endl;
+				}
+				if resultUnit == 3
+				{
+					cout << "That are " << endl;
+				}
+				if resultUnit == 4
+				{
+					cout << "Too big result" << endl;
+				}
+				if resultUnit == 5
+				{
+					cout << "Too big result" << endl;
+				}
+				if resultUnit == 6
+				{
+					cout << "Too big result" << endl;
+				}
+				if resultUnit == 7
+				{
+					cout << "Too big result" << endl;
+				}
+				if resultUnit == 8
+				{
+					cout << "Too big result" << endl;
+				}
+				if resultUnit == 9
+				{
+					cout << "Too big result" << endl;
+				}
 			}
 		}
 		else
