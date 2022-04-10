f47a6de4c36dd52aca2eae256ef66c035b720f1b
easy <60936497+ezberlin@users.noreply.github.com>
2022-04-10 16:28:39 +0200

    work with strings and logical operators

--- calculator.cpp
+++ calculator.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int again {1};
 int main()
@@ -8,47 +9,138 @@ int main()
 		char operation {0};
 		double firstNumber {0};
 		double secondNumber {0};
+		char logOperator {0};
 
 		cout << "Enter first number: ";
-		cin >> firstNumber;
-		cout << "Enter operation (addition: +, subtraction: -, multiplication: *, division: /, test of relation: =, square: ^): ";
+		string input1, input2;
+		cin >> input1;
+		cout << "Enter operation (addition: +, subtraction: -, multiplication: *,"
+		" division: /, test of relation: =, square: ^, logical operators: ?): ";
 		cin >> operation;
-		if (operation != 94)
+		if (operation == 63)
+		{
+			cout << "Enter logical operator (not: x, and: +, or: /, xor: X): ";
+			cin >> logOperator;
+		}
+		if (operation != 94 && logOperator != 120)
 		{	
 			cout << "Enter second number: ";
-			cin >> secondNumber;
+			cin >> input2;
 		}
 
 		if (operation == 43) //addition
 		{
-			cout << "The result is " << firstNumber + secondNumber << endl;
+			try
+			{
+				firstNumber = stod(input1);
+				secondNumber = stod(input2);
+				cout << "The result is " << firstNumber + secondNumber << endl;
+			}
+			catch(exception e)
+			{
+				cout <<"Invalid inputs"<<endl;
+			}
 		}
 		else if (operation == 45) //subtraction
 		{
-			cout << "The result is " << firstNumber - secondNumber << endl;
+			try
+			{
+				firstNumber = stod(input1);
+				secondNumber = stod(input2);
+				cout << "The result is " << firstNumber - secondNumber << endl;
+			}
+			catch(exception e)
+			{
+				cout <<"Invalid inputs"<<endl;
+			}
 		}
 		else if (operation == 42) //multiplication
 		{
-			cout << "The result is " << firstNumber * secondNumber << endl;
+			try
+			{
+				firstNumber = stod(input1);
+				secondNumber = stod(input2);
+				cout << "The result is " << firstNumber * secondNumber << endl;
+			}
+			catch(exception e)
+			{
+				cout <<"Invalid inputs"<<endl;
+			}
 		}
-		else if (operation == 47 && secondNumber != 0) //division
+		else if (operation == 47 ) //division
 		{
-			double result {firstNumber / secondNumber};
-			cout << "The result is " << result << endl;
+			try
+			{
+				firstNumber = stod(input1);
+				secondNumber = stod(input2);
+				if (secondNumber != 0)
+				{ 
+					cout << "The result is " << firstNumber / secondNumber << endl;
+				}
+				else
+				{
+					cout << "Cannot divide by 0" << endl;
+				}
+			}
+			catch(exception e)
+			{
+				cout << "Invalid inputs" << endl;
+			}
 		}
 		else if (operation == 61) //test of equality
 		{
-			if (firstNumber == secondNumber)
+			try
 			{
-				cout << "The first number equals the second" << endl;
+				firstNumber = stod(input1);
+				secondNumber = stod(input2);
+				if (firstNumber == secondNumber)
+				{
+					cout << "The first number equals the second" << endl;
+				}
+				if (firstNumber < secondNumber)
+				{
+					cout << "The first number is less than the second" << endl;
+				}
+				if (firstNumber > secondNumber)
+				{
+					cout << "The first number is greater than the second" << endl;
+				}
 			}
-			if (firstNumber < secondNumber)
+			catch (...)
 			{
-				cout << "The first number is less than the second" << endl;
+				cout << "Invalid inputs" << endl;
+			} 
+		}
+		else if (operation == 63) //logical operator
+		{ 
+			bool x,y;
+			try
+			{
+				y = (bool) stoi(input1);
+				if (logOperator != 120)
+				{
+					x = (bool) stoi(input2);
+				}
+				if (logOperator == 120) //not
+				{
+					cout << "The result is " << (!y) << endl;
+				}
+				else if (logOperator == 43) //and
+				{
+					cout << "The result is " << (y && x) << endl ;
+				}
+				else if (logOperator == 47) //or
+				{
+					cout << "The result is " << (x || y) << endl;
+				}
+				if (logOperator == 88) //xor
+				{
+					cout << "The result is " << ((y && (!x) ) || (!y && x)) << endl;
+				}
 			}
-			if (firstNumber > secondNumber)
+			catch (...)
 			{
-				cout << "The first number is greater than the second" << endl;
+				cout << "Invalid inputs" << endl;
 			}
 		}
 		else if (operation == 94) //square
