6174cbfa70667cda24f8150c88f0d55d6403d205
easy <60936497+ezberlin@users.noreply.github.com>
2022-05-22 12:52:58 +0200

    l61 multiplier and adder

---
+++ l61.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    cout << "Enter two integers: " << endl;
+    int num1 = 0, num2 = 0;
+    cin >> num1;
+    cin >> num2;
+    
+    cout << "Enter \'m\' to multiply, anything else to add: ";
+    char userSelection = '\0';
+    cin >> userSelection;
+    
+    int result = 0;
+    if (userSelection == 'm')
+    {
+        result = num1 * num2;
+    }
+    else
+    {
+        result = num1 + num2;
+    }
+    cout << "result is: " << result << endl;
+    return 0;
+}
